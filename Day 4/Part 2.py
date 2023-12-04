220ed6f6565fa216846a3aa488c4a1fff18cf4d3
jrdonat <jrddonat@gmail.com>
2023-12-04 11:31:03 -0500

    Part 2 Solution

--- Day 4/Part 2.py
+++ Day 4/Part 2.py
@@ -1,6 +1,3 @@
-import re
-#* Example line: Card   1:  8 86 59 90 68 52 55 24 37 69 | 10 55  8 86  6 62 69 68 59 37 91 90 24 22 78 61 58 89 52 96 95 94 13 36 81
-
 scratchCards = [1]
 
 with open("Day 4\\input.txt", "r") as file:
@@ -12,12 +9,11 @@ with open("Day 4\\input.txt", "r") as file:
         winningNumbers = [int(i) for i in winningNumbers if i != ""]
         myNumbers = line[line.index(" | ")+3:].split(" ")
         myNumbers = [int(i) for i in myNumbers if i != ""]
-        matches = {}
-        for _ in range(scratchCards[i]):
-            for number in myNumbers:
-                if number in winningNumbers:
-                    try:
-                        matches[number] += 1
-                    except:
-                        matches[number] = 1
-        
+        matches = 0
+        for number in myNumbers:
+            if number in winningNumbers:
+                matches += 1
+        for j in range(matches):
+            if i+j+1 < len(scratchCards):
+                scratchCards[i+j+1] += 1*scratchCards[i]
+    print(sum(scratchCards))
